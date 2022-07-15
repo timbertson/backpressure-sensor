b6f49d5a19eeae9efe447c86e3b61ab67e9273e0
Tim Cuthbertson <tim@gfxmonk.net>
2022-07-15 10:54:32 +1000

    bump chored

--- choredefs/index.ts
+++ choredefs/index.ts
@@ -1,3 +1,3 @@
-import { defaultDockerOptions, default as Scala } from 'https://raw.githubusercontent.com/timbertson/chored-timbertson/fdab89042fab1b5775c1b3560757bbcb647fb186/lib/scala.ts#main'
+import { defaultDockerOptions, default as Scala } from 'https://raw.githubusercontent.com/timbertson/chored-timbertson/b81b414a6bc1b3fe50fc455a10695d0129af7cbb/lib/scala.ts#main'
 
 export default Scala({ repo: 'backpressure-sensor' })
